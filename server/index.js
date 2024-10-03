7ba32b5ec888d5b74662345a4c652bee43c77cb7
Ezequiel Neistadt <94148797+eNeistadt@users.noreply.github.com>
2024-10-02 23:47:11 -0300

    endpoint login ok

--- server/index.js
+++ server/index.js
@@ -1,29 +1,23 @@
-const mysql = require('mysql2');
+import express from "express";
+import { router } from "./src/routes/hello.js";
+import {authRouter} from "./src/routes/auth.js"
+import cors from 'cors'
+import mysql from "mysql2";
 
-// Crea la conexión con tus credenciales
-const connection = mysql.createConnection({
-    host: "database-1.c7rxooynh95g.us-east-1.rds.amazonaws.com",
-    port: "3306",
-    user: "admin",
-    password: "password",
-    database: "my_db"
+const app = express();
+app.use(express.json());
+
+
+//test
+app.use(cors())
+
+
+app.use("/api", router);
+app.use("/api", authRouter);
+
+
+// app.use("/test")
+
+app.listen(4000, () => {
+  console.log("Server on port 4000");
 });
-
-// Conéctate a la base de datos
-connection.connect((err) => {
-  if (err) {
-    console.error('Error conectando a la base de datos:', err.stack);
-    return;
-  }
-  console.log('Conectado a la base de datos.');
-});
-
-connection.query('SELECT * FROM usuarios', (err, results, fields) => {
-    if (err) {
-      console.error('Error al hacer SELECT:', err.stack);
-      return;
-    }
-    console.table(results);  // Muestra los resultados en formato tabla
-  });
-// Cerrar la conexión
-connection.end();
---
+++ server/src/routes/auth.js
@@ -0,0 +1,55 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { connection } from "../../config/db.js";
+import  {sendMail}  from "../middleware/nodemailer.js"; // Importa el servicio de correo
+
+export const authRouter = express.Router();
+
+const JWT_SECRET = `da2`
+
+authRouter.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    console.log(email);
+  
+    // Verificar que los campos no estén vacíos
+    if (!email || !password) {
+      return res.status(400).json({ msg: "Por favor complete todos los campos" });
+    }
+  
+    // Consulta para obtener el usuario por el nombre de usuario (email)
+    const query = `SELECT * FROM usuarios WHERE usuario = ?`;
+    connection.query(query, [email], (err, results) => {
+      if (err || results.length === 0) {
+        console.log(err);
+        return res.status(400).json({ msg: "Usuario no encontrado" });
+      }
+  
+      const user = results[0];
+  
+      // Verificar si la contraseña es correcta
+      if (password !== user.password) {
+        return res.status(400).json({ msg: "Contraseña incorrecta" });
+      }
+  
+      // Si la contraseña es correcta, devolver el rol del usuario
+      const { rol } = user;
+      res.status(200).json({ msg: "Login exitoso", rol });
+    });
+  });
+  
+
+
+authRouter.get('/users', (req, res) => {
+  const query = 'SELECT id, nombre, email, rol FROM usuarios';
+  connection.query(query, (err, results) => {
+      if (err) {
+          return res.status(500).json({ msg: "Error al obtener los usuarios" });
+      }
+
+      // Enviar la lista de usuarios en formato JSON
+      res.status(200).json(results);
+  });
+});
+
+
